af4dabc122130ec0525a86f65e43651e68efbde4
Rafael282828 <65774986+Rafael282828@users.noreply.github.com>
2021-05-30 11:08:50 +0100

    Update ConfirmationC.cpp

--- ConfirmationC.cpp
+++ ConfirmationC.cpp
@@ -12,7 +12,7 @@ int main()
     string a; //Creating variable string
     
     cout << "Add: (So) to confirm: ";
-    cin >> a; //Add input
+    cin >> a; //Add input;
     
     if (a == "So"){ //Add conditional
         cout << "confirmed!";  //Print in output if is a correct
