3afa18e6f3e2d9aeed87850cfa82bf467d2192bf
Rafael282828 <65774986+Rafael282828@users.noreply.github.com>
2021-05-30 11:45:12 +0100

    Add files via upload

---
+++ Count_Regressive_Loop_For.cpp
@@ -0,0 +1,24 @@
+
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int q,a;
+
+    cout << "Add number, please: ";
+    
+    cin >> a;
+    
+    cout << "Add a number greater than the previous one: ";
+    
+    cin >> q;
+    
+    for (int i = a; i<=q; i++){
+        cout << i << endl;
+    }
+
+    return 0;
+}
