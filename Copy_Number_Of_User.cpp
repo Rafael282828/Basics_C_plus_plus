535eb087d277620058d5f4d4e70fd7c18c17c78e
Rafael282828 <65774986+Rafael282828@users.noreply.github.com>
2021-05-30 08:55:48 +0100

    Add files via upload

---
+++ Copy_Number_Of_User.cpp
@@ -0,0 +1,16 @@
+#include "iostream"
+
+using namespace std;
+int main()
+{
+    int h; //Creating int variable
+    int a;
+    cout << "Insert a number: "; //This so that the output that better
+    cin >> h
+    cout << "Insert other number: ";
+    cin >> a; //cin is to make the user put a number, string ...
+    cout << "Your numbers are: "<< h;
+    cout << " and "<< a;
+    return 0;
+}
+
