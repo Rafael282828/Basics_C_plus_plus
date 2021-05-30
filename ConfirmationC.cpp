72a9a7f5fae2817150ac748c80e57d941789ce82
Rafael282828 <65774986+Rafael282828@users.noreply.github.com>
2021-05-30 09:44:01 +0100

    Add files via upload

---
+++ ConfirmationC.cpp
@@ -0,0 +1,29 @@
+
+
+
+#include "iostream"
+
+using namespace std;
+
+
+int main()
+{
+    
+    string a;
+    
+    cout << "Add: (So) to confirm: ";
+    cin >> a;
+    
+    if (a == "So"){
+        cout << "confirmed!";
+    }
+    else{
+        cout << "Incorrect confirmation";
+    }
+
+ return 0;
+}
+    
+    
+
+    
